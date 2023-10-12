29aa1f9238598b5ced490cb2328f1e59978347fc
Nikhil Kumar <nk.kr001@gmail.com>
2023-10-12 08:18:54 +0530

    first commit

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener('push', event => {
+    const options = {
+      body: event.data.text(),
+    };
+  
+    event.waitUntil(
+      self.registration.showNotification('Push Notification', options)
+    );
+  });
+  
